c75a9155a6df12aa7faae661358503dbeed8f839
[051bruno] <[051brunobueno@gmail.com]>
2022-04-26 12:01:30 -0300

    Consultando dados do MYSQL na rota de Administração do menu

--- inc/admin.js
+++ inc/admin.js
@@ -1,4 +1,47 @@
+var conn = require ('./db')
+
+//const { ConnectionTimeoutError } = require("redis");
+
 module.exports = {
+
+    dashboard(){
+
+        return new Promise((resolve, reject) =>{
+
+            conn.query(`
+                SELECT
+                    (SELECT
+                        COUNT(*)
+                    FROM
+                        tb_contacts) AS nrcontacts, 
+                    (SELECT
+                        COUNT(*)
+                    FROM
+                        tb_menus) AS nrmenus,   
+                    (SELECT
+                        COUNT(*)
+                    FROM
+                        tb_reservations) AS nrreservations,   
+                    (SELECT
+                        COUNT(*)
+                    FROM
+                        tb_users) AS nrusers;
+            
+            
+            `, (err, results) => {
+
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(results[0])
+                }
+
+            });
+
+
+        })
+
+    },
 
     getParams(req, params){
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var users = require("./../inc/users")
 var admin = require("./../inc/admin")
+var menus = require("./../inc/menus")
 var router = express.Router();
 
 router.use(function(req, res, next){ 
@@ -31,8 +32,16 @@ router.use(function(req, res, next){
 
 router.get("/", function(req, res, next) {
 
-      res.render("admin/index", admin.getParams(req));
+      admin.dashboard().then(data => {
 
+        res.render("admin/index", admin.getParams(req, {
+          data
+        }));
+     }).catch(err => {
+
+      console.error(err);
+
+     })
 });
   
 router.post("/login", function(req, res, next) {
@@ -78,7 +87,13 @@ router.get("/emails", function(req, res, next) {
  
 router.get("/menus", function(req, res, next) {
       
-  res.render("admin/menus", admin.getParams(req));
+  menus.getMenus().then(data => {
+
+    res.render("admin/menus", admin.getParams(req, {
+      data
+    }));
+
+  })
 
 });
 
